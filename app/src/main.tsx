eff5e46743db5dbb9e4820850fbde313c2fad773
ActiveSpaceSRC <space@activespacescience.com>
2026-05-10 13:33:29 -0700

    app: import print.css globally so window.print() picks it up everywhere

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -5,6 +5,7 @@ import { LicenseInfo } from '@mui/x-license';
 import { LocalizationProvider } from '@mui/x-date-pickers-pro';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import './styles/theme.css';
+import './styles/print.css';
 import { App } from './App';
 import { brixTheme } from './lib/muiTheme';
 import { ToastProvider } from './lib/toast';
